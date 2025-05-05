3905405749b2288e83edc43a39b50f1738bb0186
Sunamra <alice@mail.com>
2025-05-05 22:05:37 +0530

    Added Dropbox redirect to root/route

--- apps/root/routes/index.js
+++ apps/root/routes/index.js
@@ -5,4 +5,8 @@ router.route('/').get((_, res) => {
 	res.status(200).send('Hello from Express');
 });
 
+router.route('/videotools').get((_, res) => {
+	res.redirect('https://www.dropbox.com/scl/fi/8x9gn69ja0ah4lsv37rct/VideoTools.zip?rlkey=gi7bubvd8aapjue6g8togy6he');
+});
+
 module.exports = router;
