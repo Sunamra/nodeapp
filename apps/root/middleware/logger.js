1526573ae147717dc8b225607114f245022951d2
Sunamra <alice@mail.com>
2025-05-20 12:37:35 +0530

    Add root/ controllers, middlewares, utilities and store

--- apps/root/middleware/logger.js
+++ apps/root/middleware/logger.js
@@ -1,6 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+const { rootLogDir } = require('../utils/constants');
+
+const writeLog = (req, timestamp) => {
+
+	const log = `${(new Date(timestamp)).toLocaleString('en-IN', {
+		hour12: false,
+	})}  -  ${req.ip}  -  ${req.method} ${req.url} ${(req.protocol).toUpperCase()}/${req.httpVersion}  -  ${req.headers['user-agent']}\n`;
+
+	fs.appendFile(path.join(rootLogDir, 'access.log'), log, (err) => {
+		if (err) console.error('Logging error :', err);
+	});
+
+	// console.log(log);
+};
+
 module.exports = (req, _, next) => {
-	// console.log((new Date()).toLocaleString('en-IN', {
-	// 	hour12: false,
-	// }), " | ", req.ip, " | ", req.headers['user-agent']);
+
+	fs.mkdir(rootLogDir, { recursive: true });
+	writeLog(req, Date.now());
 	next();
 };
