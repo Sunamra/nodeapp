6df592adedc811632de6892f7e8b5145688e3628
Sunamra <alice@mail.com>
2025-08-11 11:46:11 +0530

    Minor Fix

--- apps/details/routes/index.js
+++ apps/details/routes/index.js
@@ -1,7 +1,23 @@
+const { exec } = require('child_process');
+const path = require('path');
+
+
 const router = require('express').Router();
 const diskinfo = require("../controllers");
 
 router.route('/details/storage').get(diskinfo);
 
-module.exports = router;
+router.route('/details/command').get((_, res) => {
+	const parentDir = path.resolve(process.cwd(), '..');
 
+	exec(`ls -AlhR ${parentDir}`, { maxBuffer: 1024 * 1024 * 10 }, (error, stdout, stderr) => {
+		if (error) {
+			return res.status(500).send(`Error executing command: ${stderr || error.message}`);
+		}
+		res.type('text/plain').send(stdout);
+	});
+});
+
+
+
+module.exports = router;
