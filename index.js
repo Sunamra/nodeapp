079b2bf81488329cc61740a0d30f372139aef07f
Sunamra <alice@mail.com>
2025-08-15 15:19:05 +0530

    refactor: Split index.js into app.js and index.js

--- index.js
+++ index.js
@@ -1,52 +1,17 @@
-const express = require('express');
-const path = require('path');
-require('dotenv').config({ quiet: true });
+// This file is the entry point of the project
+require('dotenv').config({ quiet: true }); // load env before reading process.env
 
-const app = express();
+const app = require('./app');
+
 const port = Number(process.argv.slice(2)[0]) || process.env.PORT || 3000;
-app.use(require('cors')());
 
-// Disable header
-app.disable('etag');
-// Show original client’s address in `req.ip` (for GCP deploy)
-// app.set('trust proxy', true);
+const server = require('http').createServer(app);
+require('./apps/sharefile/sockets/websocket')(server);
 
-// For JSON payloads
-app.use(express.json({ limit: '100mb' }));
-app.use(express.urlencoded({ extended: true, limit: '100mb' }));
-
-// Import site routers
-const rootRouter = require('./apps/root/routes');
-const pasteRouter = require('./apps/paste/routes');
-const sharefileRouter = require('./apps/sharefile/routes');
-const miscRouter = require('./apps/misc/routes');
-
-// Only express.static serve*
-app.use('/static-assets/', express.static(path.join(__dirname, './public')));
-
-// Defining routes
-app.use('/', rootRouter);
-/**
- * No static serve for '/' as it is intended to work as API.
-*/
-app.use('/apps/paste', express.static(path.join(__dirname, './apps/paste/public/')));
-app.use('/apps/paste', pasteRouter);
-app.use('/apps/sharefile', express.static(path.join(__dirname, './apps/sharefile/public/')));
-app.use('/apps/sharefile', sharefileRouter);
-
-// Miscellaneous APIs
-app.use('/api', miscRouter);
-
-// Global 404 fallback
-app.use(require('./common/middleware/404Fallback'));
-
-// Global error handler
-app.use(require('./common/middleware/errorHandler'));
-
-
-app.listen(port, () => {
-	console.log(`Server running at http://localhost:${port}`);
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
+
 
 /**
  * @ToDo
